502879ec3f0a3c294031e799bf5715104379566b
zhuhoulin <>
2018-07-30 18:14:25 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-YWDialog_Tests/Pods-YWDialog_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YWDialog_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YWDialog_Tests
+@end
---
+++ Example/Pods/Target Support Files/YWDialog/YWDialog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YWDialog : NSObject
+@end
+@implementation PodsDummy_YWDialog
+@end
